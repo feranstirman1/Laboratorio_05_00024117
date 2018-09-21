6dccb7b93cad9c6526685071cc6734e9b773a5e4
feranstirman1 <00024117@uca.edu.sv>
2018-09-21 10:59:46 -0600

    completo

--- Ejercicios/Ejercicio_3.js
+++ Ejercicios/Ejercicio_3.js
@@ -1,3 +1,14 @@
-function Fibonacci(n){
-    
+function fibonacci(n){
+    var a=0;
+    var b=1;
+    var resultado = a + ", ";
+ 
+    for(i=0; i<n-1;i++){
+        var numeroTemporal=a;
+        a=b;
+        b=numeroTemporal+b;
+ 
+        resultado = resultado + a + ", ";
+    }
+    return resultado;
 }
