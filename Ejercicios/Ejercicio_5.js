f21e7e662459ca0e570f3d35ea893ca4c6d09af7
feranstirman1 <00024117@uca.edu.sv>
2018-09-21 11:09:11 -0600

    completo

--- Ejercicios/Ejercicio_5.js
+++ Ejercicios/Ejercicio_5.js
@@ -2,7 +2,7 @@ function areaCirculo(radio){
     if(radio==0 || radio<0){
         return -1;
     }else{
-        var area= Math.PI * (radio**2);
+        var area= Math.PI * (radio*radio);
         return area;
     }
 }
